08f82be64ff221ed6a0e73aa3c1a51ca0a345a79
Valerie Chan <vchan36@my.centennialcollege.ca>
2018-04-17 23:51:10 -0400

    Initial commit

---
+++ AIFlocking_BasenUpgrade/Assets/Scripts/flock.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flock : MonoBehaviour {
+    public float speed = 0.001f;
+    float rotationSpeed = 2.0f;
+    Vector2 averageHeading;
+    Vector2 averagePosition;
+
+    float neighbourDistance = 2.0f;
+
+    bool turning = false;
+
+	// Use this for initialization
+	void Start () {
+        speed = Random.Range(0.000001f, 0.005f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector2.Distance(transform.position, Vector2.zero) >= globalFlock.tankSize)
+        {
+            turning = true;
+        }
+        else turning = false;
+
+        if(turning)
+        {
+            Vector3 direction = Vector3.zero - transform.position;
+            this.GetComponent<Rigidbody2D>().AddForce(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                  Quaternion.LookRotation(direction),
+                                                  rotationSpeed * Time.deltaTime);
+        }
+        transform.position = Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), speed);
+
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+        float rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotation_z);
+    }
+}
---
+++ AIFlocking_BasenUpgrade/Assets/Scripts/globalFlock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class globalFlock : MonoBehaviour {
+    public GameObject playerPrefab;
+    public static int tankSize = 6;
+
+    static int numPlayer = 30;
+    public static GameObject[] allPlayers = new GameObject[numPlayer];
+
+    public static Vector2 goalPos = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < numPlayer; i++)
+        {
+            Vector2 pos = new Vector2(Random.Range(-tankSize, tankSize),
+                                      Random.Range(-tankSize, tankSize));
+            allPlayers[i] = (GameObject)Instantiate(playerPrefab, pos, Quaternion.identity);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Random.Range(0, 10000) < 50)
+        {
+            goalPos = new Vector2(Random.Range(-tankSize, tankSize),
+                                  Random.Range(-tankSize, tankSize));
+        }
+	}
+}
